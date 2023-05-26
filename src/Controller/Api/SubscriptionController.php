798dd56b08fadf6f566cef7b545451cb28579777
trndo <trndogv@gmail.com>
2023-05-26 18:08:15 +0300

    API-2: remove typo

--- src/Controller/Api/SubscriptionController.php
+++ src/Controller/Api/SubscriptionController.php
@@ -9,15 +9,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api')]
 class SubscriptionController
 {
     public function __construct(
         private DataPersisterInterface $dataPersister,
-        private ValidatorInterface $validator,
     ) {}
 
     #[Route('/subscribe', methods: 'POST')]
